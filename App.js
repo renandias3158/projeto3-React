315fa781b4b3b9b9814757672154c8b8034093ea
aluno <aluno@ifpe>
2026-02-25 08:53:56 -0300

    number 1

--- App.js
+++ App.js
@@ -1,21 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { View, Text, Button, Image, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Stack = createNativeStackNavigator();
+
+function HomeScreen({ navigation }) {
+return (
+<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+<Text>Home Screen</Text>
+<Button title='Images' onPress={() => navigation.navigate('Images')}>
+</Button>
+</View>
+);
+}
+
+
+function Images({ navigation }) {
+return (
+<SafeAreaProvider>
+<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+<Text>.</Text>
+    <SafeAreaView style={styles.container}>
+<Image
+      style={styles.logo}
+        source={{
+           uri: 'https://limbuscompany.wiki.gg/wiki/LCE_E.G.O::Lantern_Yi_Sang'
+        }}
+      />
+      </SafeAreaView>
+</View>
+</SafeAreaProvider>
+);
+}
+
+function LoginScreen({ navigation }) {
+return (
+<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+<Text>Login Screen</Text>
+<Button title='Home' onPress={() => navigation.navigate('Home')}>
+</Button>
+<Button title='Forgot password' onPress={() => navigation.navigate('Forgot password')}>
+</Button>
+</View>
+);
+}
+
+function ForgotPassword({ navigation }) {
+return (
+<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+<Text>Forgot Password</Text>
+</View>
+);
+}
+
+function App() {
+return (
+<NavigationContainer>
+<Stack.Navigator>
+<Stack.Screen name="Login" component={LoginScreen} />
+<Stack.Screen name="Home" component={HomeScreen} />
+<Stack.Screen name="Forgot password" component={ForgotPassword} />
+<Stack.Screen name="Images" component={Images} />
+</Stack.Navigator>
+</NavigationContainer>
+);
 }
 
 const styles = StyleSheet.create({
+  logo: {
+    width: 200,
+    height: 200,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  }
 });
+
+export default App;

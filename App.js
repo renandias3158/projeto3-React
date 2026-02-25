f88411412a23c674267d582cb97c86ca5ee0a04b
aluno <aluno@ifpe>
2026-02-25 10:07:58 -0300

    number 2

--- App.js
+++ App.js
@@ -1,56 +1,87 @@
 import * as React from 'react';
-import { View, Text, Button, Image, StyleSheet } from 'react-native';
+import { View, Text, Button, Image, StyleSheet, TouchableOpacity,TextInput} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 const Stack = createNativeStackNavigator();
-
+  
 function HomeScreen({ navigation }) {
 return (
 <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
 <Text>Home Screen</Text>
-<Button title='Images' onPress={() => navigation.navigate('Images')}>
+<Button title='Contatos' onPress={() => navigation.navigate('Contacts')}>
 </Button>
 </View>
 );
 }
 
 
-function Images({ navigation }) {
-return (
-<SafeAreaProvider>
-<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-<Text>.</Text>
-    <SafeAreaView style={styles.container}>
-<Image
-      style={styles.logo}
-        source={{
-           uri: 'https://limbuscompany.wiki.gg/wiki/LCE_E.G.O::Lantern_Yi_Sang'
-        }}
-      />
-      </SafeAreaView>
-</View>
-</SafeAreaProvider>
-);
+
+function Contacts({ navigation }) {
+
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity onPress={() => navigation.navigate('CadastrarContato')}>
+          <Image
+            source={{ uri: 'https://cdn-icons-png.flaticon.com/512/8922/8922789.png' }}
+            style={{ width: 28, height: 28, marginRight: 15 }}
+          />
+        </TouchableOpacity>
+      ),
+    });
+  }, []);
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text onPress={() => navigation.navigate('AlterarContato')}>Pessoa 1</Text>
+      <Text onPress={() => navigation.navigate('AlterarContato')}>Pessoa 2</Text>
+      <Text onPress={() => navigation.navigate('AlterarContato')}>Pessoa 3</Text>
+    </View>
+  );
 }
 
 function LoginScreen({ navigation }) {
 return (
 <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
 <Text>Login Screen</Text>
-<Button title='Home' onPress={() => navigation.navigate('Home')}>
+<Button title='Login' onPress={() => navigation.navigate('Home')}>
 </Button>
-<Button title='Forgot password' onPress={() => navigation.navigate('Forgot password')}>
+<Button title='Cadastrar' onPress={() => navigation.navigate('signIn')}>
 </Button>
+</View>
+);  
+}
+
+function SignIn({ navigation }) {
+return (
+<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+<Text>Cadastro</Text>
+<Button title='Salvar'></Button>
 </View>
 );
 }
 
-function ForgotPassword({ navigation }) {
+function AlterarContato({ navigation }) {
 return (
 <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-<Text>Forgot Password</Text>
+<Text>Alterar Contato</Text>
+<Button title='Alterar'></Button>
+<Button title='Excluir'></Button>
+</View>
+);
+}
+function CadastrarContato({ navigation }) {
+return (
+<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+<Text>Nome</Text>
+<TextInput style={styles.input} placeholder="Nome"/>
+<Text>Email</Text>
+<TextInput style={styles.input} placeholder="Email"/>
+<Text>Telefone</Text>
+<TextInput style={styles.input} placeholder="Telefone"/>
+<Button title='salvar'></Button>
 </View>
 );
 }
@@ -61,8 +92,10 @@ return (
 <Stack.Navigator>
 <Stack.Screen name="Login" component={LoginScreen} />
 <Stack.Screen name="Home" component={HomeScreen} />
-<Stack.Screen name="Forgot password" component={ForgotPassword} />
-<Stack.Screen name="Images" component={Images} />
+<Stack.Screen name="signIn" component={SignIn} />
+<Stack.Screen name="Contacts" component={Contacts} />
+<Stack.Screen name="AlterarContato" component={AlterarContato} />
+<Stack.Screen name="CadastrarContato" component={CadastrarContato} />
 </Stack.Navigator>
 </NavigationContainer>
 );
@@ -75,6 +108,12 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   }
 });
 
